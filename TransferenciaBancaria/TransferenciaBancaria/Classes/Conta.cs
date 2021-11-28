08d006343ea66eabf28f91ff5ffe541e74a948d0
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2021-11-28 16:59:33 -0300

    Implmentação dos métodos de Sacar, Depositar e Transferir

--- TransferenciaBancaria/TransferenciaBancaria/Classes/Conta.cs
+++ TransferenciaBancaria/TransferenciaBancaria/Classes/Conta.cs
@@ -23,5 +23,43 @@ namespace TransferenciaBancaria.Classes
             Credito = credito;
             Nome = nome;
         }
+
+        public bool Sacar(double valorASacar)
+        {
+            //Validação de saldo suficente
+            if(this.Saldo - valorASacar < (this.Credito * -1))
+            {
+                System.Console.WriteLine("Saldo Insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorASacar;
+            System.Console.WriteLine("O saldo atual da conta {0} é {1}", this.Nome, this.Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            System.Console.WriteLine("O saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTrasnferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTrasnferencia))
+            {
+                contaDestino.Depositar(valorTrasnferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.tipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito + " | ";
+            return retorno;
+        }
     }
 }
