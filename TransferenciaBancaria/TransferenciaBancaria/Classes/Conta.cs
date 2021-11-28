cb89611542c83563e623c74703c41b9e054a32cc
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2021-11-28 16:45:27 -0300

    Criação das primeiras classes e métodos da aplicação

---
+++ TransferenciaBancaria/TransferenciaBancaria/Classes/Conta.cs
@@ -0,0 +1,27 @@
+﻿using TransferenciaBancaria.Enum;
+
+namespace TransferenciaBancaria.Classes
+{
+    public class Conta
+    {
+        private TipoConta tipoConta { get; set; }
+
+        public double Saldo { get; set; }
+        
+        public double Credito { get; set; }
+
+        public string Nome { get; set; }
+
+        public Conta()
+        {
+        }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.tipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+    }
+}
---
+++ TransferenciaBancaria/TransferenciaBancaria/Enum/TipoConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransferenciaBancaria.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
